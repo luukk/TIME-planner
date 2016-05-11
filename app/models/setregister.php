9983c168138ee7c80cef9291f71f56613c8e75ca
luukk <klaverluuk@gmail.com>
2016-05-11 20:51:42 +0200

    fixed bug for parsing user id

--- app/models/setregister.php
+++ app/models/setregister.php
@@ -3,19 +3,23 @@ if(isset($_GET['username']) || isset($_GET['password'])){
   if (!empty($_GET['username']) && !empty($_GET['password'])) {
     $username = $_GET['username'];
     $password = $_GET['password'];
-    $query = "select * from user where username='$username' AND password='$password'";
+    $query = "select * from user where username='$username'";
     $check = $mysqli->query($query);
     $rowcount = mysqli_num_rows($check);
+
     if($rowcount == 0){
       session_start();
        $result = $mysqli->query('INSERT INTO user (username,password) VALUES ("'.$username.'","'.$password.'")');
-       setcookie('user',$username.','.$id,time() + (86400 *30), '/');
-       header('Location: index.php');
+       $uservar = $mysqli->query("select * from user where username='$username' AND password='$password'");
+
+       $userid = $uservar->fetch_assoc();
+       setcookie('user',$username.','.$userid['userid'],time() + (86400 *30), '/');
+      header('Location: index.php');
     }else{
-      echo 'deze ding is al in gebruik';
+      echo 'this username already exists';
     }
   }else{
-    echo 'vul beide velden in';
+    echo 'fill both fields';
   }
 }
  ?>
