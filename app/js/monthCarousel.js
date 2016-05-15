88324a781d98bdedfcf4f3fc1c65ff2638f0374f
luukk <klaverluuk@gmail.com>
2016-05-15 16:47:52 +0200

    solved a wrong named variable

--- app/js/monthCarousel.js
+++ app/js/monthCarousel.js
@@ -7,25 +7,29 @@ window.onload = function(){
   var LeftCenter = carouselWidth/2-blockWidth;
   var rightCenter = carouselWidth/2;
   var oldxValue = 0;
+  console.log(children[5].offsetLeft,children[5],carouselWidth);
 
-  function scrollHorizontally(e) {
-    e = window.event || e;
-    var positionInfo = carousel.getBoundingClientRect();
-    var mouseX = e.pageX - positionInfo.left;
-    if(e.pageX < oldxValue){
-      carousel.scrollLeft -= 3;
-    }else if(e.pageX > oldxValue){
-      carousel.scrollLeft += 3;
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var positionInfo = carousel.getBoundingClientRect();
+        var mouseX = e.pageX - positionInfo.left;
+        if(e.pageX < oldxValue){
+          carousel.scrollLeft += 3;
+        }else{
+          carousel.scrollLeft -= 3;
+        }
+        oldxValue =  e.pageX;
+        e.preventDefault();
     }
-    oldX =  e.pageX;
-  }
-   carousel.addEventListener("mousedown",mousedown,false);
-   carousel.addEventListener("mouseup",mouseup,false);
 
-  function mousedown() {
-     carousel.addEventListener('mousemove', scrollHorizontally,false);
-  };
-  function mouseup() {
-    carousel.removeEventListener('mousemove', scrollHorizontally,false);
-  };
+
+       carousel.addEventListener("mousedown",mousedown,false);
+       carousel.addEventListener("mouseup",mouseup,false);
+
+      function mousedown() {
+         carousel.addEventListener('mousemove', scrollHorizontally,false);
+      };
+      function mouseup() {
+        carousel.removeEventListener('mousemove', scrollHorizontally,false);
+      };
 }
