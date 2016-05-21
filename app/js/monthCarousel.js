c0aaab1f439f939bf1066058a2794d840409c87f
luukk <klaverluuk@gmail.com>
2016-05-21 16:41:53 +0200

    fix center bug

--- app/js/monthCarousel.js
+++ app/js/monthCarousel.js
@@ -26,16 +26,18 @@ window.onload = function(){
     var positions = [];
     for (var i = 0; i < children.length; i++) {
       var left = children[i].getBoundingClientRect().left - positionInfo.left;
-      var offsetCenter = center-left-65;
+      var offsetCenter = left;
       positions.push(offsetCenter);
     }
-    var startvalue = positions[0];
+    var startvalue = positions[0] < 0 ? positions[0] *-1 : positions[0];
+    console.log("startvalue",startvalue);
     var index = 0;
 
     for (var i = 0; i < positions.length; i++) {
-      var tempvar = (positions[i] < 0) ? positions[i] * -1 : positions[i]
+      var tempvar = (positions[i] < 0) ? positions[i] * -1 : positions[i];
+      console.log(tempvar);
       if(tempvar < startvalue){
-        startvalue = positions[i];
+        startvalue = tempvar;
         index = i;
       }
     }
@@ -47,23 +49,24 @@ window.onload = function(){
     var test = 1;
     setInterval(function(){
       if(temp < offset){
-        carousel.scrollLeft -= 1;
+        carousel.scrollLeft += 1;
         temp +=1;
       }else if (temp > offset){
-        carousel.scrollLeft -= -1;
+        carousel.scrollLeft += -1;
         temp -= 1;
       }
     })
   }
   function infinCarousel(){
+    var offset = setcenter();
     var test = carousel.scrollLeft > 1280-450 ? children.length : ((carousel.scrollLeft < 450)) ? 0 : null;
         load = test === 0 ? 11 : 0,
-        scrollDirection = test === 0 ? 270 : -270;
+        scrollDirection = test === 0 ? 0 : -0
     if(test != null){
       for (var i = 0; i < 2; i++) {
         children[load].style.marginRight = '4px';
         carousel.insertBefore(children[load],children[test]);
-      }
+     }
       carousel.scrollLeft += scrollDirection;
     }
   }
@@ -74,5 +77,5 @@ window.onload = function(){
   carousel.addEventListener("mousedown",mousedown,false);
   carousel.addEventListener("mouseup",setcenter,false);
   carousel.addEventListener("mouseup",infinCarousel,false);
-  infinCarousel();
+//  infinCarousel();
 }
