a8f3a3b30899746ddec9931c5626f8381898168a
luukk <klaverluuk@gmail.com>
2016-06-09 09:43:13 +0200

    fix another centering bug

--- app/js/monthCarousel.js
+++ app/js/monthCarousel.js
@@ -12,7 +12,7 @@ window.onload = function(){
 
   function setChildWidth(){
     for (var i = 0; i < children.length; i++) {
-      children[i].style.width = parseInt((carouselWidth/3 ) -4) + "px";
+      children[i].style.width = parseInt(carouselWidth/3) + "px";
     }
   }
   setChildWidth();
@@ -35,7 +35,7 @@ window.onload = function(){
     positions = [];
     for (var i = 0; i < children.length; i++) {
       var month = carousel.children[i].innerHTML;
-      var left = children[i].getBoundingClientRect().left - positionInfo.left -childInfo.width -4;
+      var left = children[i].getBoundingClientRect().left - positionInfo.left -childInfo.width;
       positions.push({month:month, offset: left});
     }
     var startvalue = positions[0].offset < 0 ? positions[0].offset *-1 : positions[0].offset;
@@ -48,45 +48,60 @@ window.onload = function(){
 
     }
   }
-    selectedMonth = positions[index+1];
-    animate(positions[index].offset);
-  }
-  function animate(offset){
-    var temp = 0 ;
-    var test = 1;
-    setInterval(function(){
-      if(temp < offset){
-        carousel.scrollLeft += 1;
-        temp +=1;
-      }else if (temp > offset){
-        carousel.scrollLeft += -1;
-        temp -= 1;
+    animate(positions,index);
+
+}
+  function animate(object,month){
+    test = 1;
+    var offset = object[month].offset;
+    var scrollCount = 0;
+    scrollInterval = setInterval(function(){
+      if(scrollCount != offset){
+        scrollCount = offset < scrollCount ? scrollCount -= 1 : scrollCount +=1;
+        if(offset < scrollCount){
+          carousel.removeEventListener('mousedown', mousedown,false);
+          carousel.removeEventListener("mouseup",setcenter,false);
+          carousel.scrollLeft -= 1;
+        }else{
+          carousel.removeEventListener('mousedown', mousedown,false);
+          carousel.removeEventListener("mouseup",setcenter,false);
+          carousel.scrollLeft += 1;
+        }
       }
-    })
+      if(scrollCount === offset){
+        clearInterval(scrollInterval);
+        selectedMonth = object[month+1];
+        console.log(selectedMonth);
+        getData();
+          carousel.addEventListener('mousedown', mousedown,false);
+          carousel.addEventListener("mouseup",setcenter,false);
+        }
+    });
+
+
   }
   function setStartPosition(){
-    setcenter(selectedMonth);
+    setcenter();
     var monthNames = ["January", "February", "March","April", "May", "June", "July","August", "September", "October","November", "December"],
         currentMonth = new Date().getMonth(),
         width = children[0].getBoundingClientRect().width;
     for (var i = 0; i < positions.length; i++) {
       if(positions[i].month === monthNames[currentMonth]){
-        carousel.scrollLeft = positions[i].offset -width;
+        carousel.scrollLeft = positions[i].offset -width ;
         selectedMonth = positions[i]
       }
     }
   }
   setStartPosition();
+
   function getData(){
     var checkMonth = selectedMonth.month;
     httpRequest = new HttpRequest();
-
     httpRequest.load("app/models/carouselOutput.php?date="+checkMonth, function(data) {
       data = JSON.parse(data);
-      document.querySelector('.earnings').innerHTML = data.earned;
+      document.querySelector('.earnings').innerHTML = "&euro;"+data.earned;
       document.querySelector('.hours').innerHTML = data.hours_worked;
     });
-
   }
   getData();
   function infinCarousel(){
@@ -105,10 +120,14 @@ window.onload = function(){
   function mousedown() {
      carousel.addEventListener('mousemove', scrollHorizontally,false);
   };
+  function mouseup(){
+
+  }
 
   carousel.addEventListener("mousedown",mousedown,false);
   carousel.addEventListener("mouseup",setcenter,false);
   carousel.addEventListener("mouseup",infinCarousel,false);
-  carousel.addEventListener("mouseup",getData,false);
+//  carousel.addEventListener("mouseup",getData,false);
+  carousel.addEventListener("mouseup",mouseup,false);
   infinCarousel();
 }
