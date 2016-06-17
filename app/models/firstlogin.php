409decefd3a6f1618ccd47344b4f0493dd16a7bf
luukk <klaverluuk@gmail.com>
2016-06-17 18:55:24 +0200

    fix showing salary screen

--- app/models/firstlogin.php
+++ app/models/firstlogin.php
@@ -6,7 +6,7 @@ $userid = $_COOKIE['userid'];
 $username = $_COOKIE['user'];
 
 $mysqli = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
-$first_login = mysqli_num_rows($mysqli->query('SELECT * FROM user WHERE userid = "'.$userid.'" AND username = "'.$username.'" AND first_login = 0'));
+$first_login = mysqli_num_rows($mysqli->query('SELECT * FROM user WHERE userid = "'.$userid.'" AND username = "'.$username.'" AND (first_login = 0 OR salary = 0)'));
 if($first_login == 1){
   $test = $mysqli->query('UPDATE user SET first_login = 1 WHERE userid = "'.$userid.'"');
 }
