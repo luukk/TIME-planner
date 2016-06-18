1b7ed17dcfb2f51c3efe73af7b18cc8b493ea10e
luukk <klaverluuk@gmail.com>
2016-06-18 14:18:39 +0200

    set salary cookie

--- app/models/cookie.php
+++ app/models/cookie.php
@@ -2,5 +2,5 @@
 $expire_date = 2147483647;
 setcookie('user',$username,$expire_date, '/');
 setcookie('userid',$userid['userid'],$expire_date,'/');
-
+setcookie('salary',$userid['salary'],$expire_date,'/');
  ?>
--- app/models/setSetup.php
+++ app/models/setSetup.php
@@ -6,6 +6,7 @@ $salary = $_GET['salary'];
 $userid = $_COOKIE['userid'];
 $mysqli = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
 $check = $mysqli->query('UPDATE user SET salary = "'.$salary.'" WHERE userid = '.$userid);
+setcookie('salary',$salary,2147483647,'/');
 if($check >=1){
   echo 'succes';
 }else{
